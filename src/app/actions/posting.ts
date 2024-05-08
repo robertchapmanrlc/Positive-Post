e10fbefd8150ae3de6a6f49804ca8e7576275edd
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-08 13:19:04 -0500

    Uses hugging face inference to classify a message

--- src/app/actions/posting.ts
+++ src/app/actions/posting.ts
@@ -1,9 +1,30 @@
 "use server";
 
+import { HfInference } from "@huggingface/inference";
+
 import { redirect } from "next/navigation";
 
+let hf: HfInference;
+
 export async function postMessage(userId: string, formData: FormData) {
-  console.log(userId);
-  console.log(formData);
+
+  const message = formData.get('message') as string;
+
+  const inferenceResponse = await runHfInference(message);
+
   redirect('/message');
+}
+
+async function runHfInference(input: string) {
+  if (!hf) {
+    hf = new HfInference(process.env.HF_TOKEN);
+  }
+
+  const modelName = "finiteautomata/bertweet-base-sentiment-analysis";
+  const inferenceResult = await hf.textClassification({
+    model: modelName,
+    inputs: input
+  });
+
+  return inferenceResult;
 }
