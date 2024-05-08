9289351b22d35eb37b9dda135db045dd6470ab4b
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-08 15:11:35 -0500

    Uses react-hot-toast to inform users of success or failure of sending the message

--- src/app/components/posit-form.tsx
+++ src/app/components/posit-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import toast from "react-hot-toast";
 import { postMessage } from "../actions/posting";
 
 export default function PositivityForm({
@@ -13,7 +14,9 @@ export default function PositivityForm({
         const result = await postMessage(formData);
 
         if (result?.error) {
-          alert(result.error);
+          toast.error(result.error);
+        } else {
+          toast.success('Positive message sent!');
         }
       }}
       className="w-full max-w-xs sm:max-w-lg md:max-w-2xl flex flex-col gap-y-5"
