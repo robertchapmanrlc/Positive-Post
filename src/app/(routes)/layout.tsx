cdd49878de33942d406ac13d6156e95890f8e0c7
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-10 19:22:47 -0500

    Tad UI fix for the routes on mobile screens

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="w-full">
+    <main className="w-full pt-8 sm:pt-0">
       <div className="absolute right-4 top-4">
         <UserButton />
       </div>
