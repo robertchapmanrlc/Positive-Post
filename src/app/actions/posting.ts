e9b2d163ea17e49f0ffab03276541be868260edc
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-28 17:28:43 -0500

    Insert user daily activity into the database when posting a message.

--- src/app/actions/posting.ts
+++ src/app/actions/posting.ts
@@ -5,33 +5,45 @@ import { auth } from "@clerk/nextjs/server";
 import { HfInference, TextClassificationOutput } from "@huggingface/inference";
 
 import db from "../../../db/drizzle";
-import { posts } from "../../../db/schema";
+import { daily_activities, posts } from "../../../db/schema";
 
 let hf: HfInference;
 
 export async function postMessage(formData: FormData) {
-
   const { userId } = auth();
 
   if (!userId) {
-    throw new Error('Unauthorized');
+    throw new Error("Unauthorized");
   }
 
-  const message = formData.get('message') as string;
+  const message = formData.get("message") as string;
 
-  const inferenceResponse: TextClassificationOutput = await runHfInference(message);
+  const inferenceResponse: TextClassificationOutput = await runHfInference(
+    message
+  );
 
   const isPositive = positiveEnough(inferenceResponse);
 
   if (!isPositive) {
     return {
-      error: 'Try a more positive message'
-    }
+      error: "Try a more positive message",
+    };
   }
 
-  await db.insert(posts).values({ userId: userId, message: message });
+  const insertPromises = [
+    await db.insert(posts).values({ userId: userId, message: message }),
+    await db
+      .insert(daily_activities)
+      .values({ userId: userId, last_sent_at: new Date() })
+      .onConflictDoUpdate({
+        target: daily_activities.userId,
+        set: { last_sent_at: new Date() },
+      }),
+  ];
 
-  redirect('/message');
+  await Promise.all(insertPromises);
+
+  redirect("/message");
 }
 
 async function runHfInference(input: string) {
@@ -42,12 +54,12 @@ async function runHfInference(input: string) {
   const modelName = "finiteautomata/bertweet-base-sentiment-analysis";
   const inferenceResult = await hf.textClassification({
     model: modelName,
-    inputs: input
+    inputs: input,
   });
 
   return inferenceResult;
 }
 
 function positiveEnough(output: TextClassificationOutput) {
-  return output[0].label === 'POS' && output[0].score >= 0.75;
+  return output[0].label === "POS" && output[0].score >= 0.75;
 }
