699027fab3496ce57ebe97489a9772ba9bab753c
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-28 16:15:59 -0500

    Added daily activity table to the database

--- db/schema.ts
+++ db/schema.ts
@@ -6,3 +6,9 @@ export const posts = pgTable("posts", {
   message: text("message").notNull(),
   createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
+
+export const daily_activities = pgTable("daily_activities", {
+  daily_activity_id: serial('id').primaryKey(),
+  userId: text("userId").notNull().unique(),
+  last_sent_at: timestamp('last_sent_at').notNull().defaultNow()
+});
